5796fa8c3ae00770dedca5c86ed040b1a6c26d38
Tuyen2k <tuyen4835@gmail.com>
2023-08-30 16:41:04 +0700

    display product

--- src/main/java/com/example/case_study_md3_demo/DAO/iplm/ProductDAO.java
+++ src/main/java/com/example/case_study_md3_demo/DAO/iplm/ProductDAO.java
@@ -2,13 +2,44 @@ package com.example.case_study_md3_demo.DAO.iplm;
 
 import com.example.case_study_md3_demo.DAO.IProductDAO;
 import com.example.case_study_md3_demo.model.Product;
+import com.example.case_study_md3_demo.myConnection.MyConnection;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDAO implements IProductDAO {
+    MyConnection myConnection = MyConnection.getMyConnection();
+    Connection connection;
+    public ProductDAO(){
+        connection = myConnection.getConnection();
+    }
     @Override
     public List<Product> findAll() {
-        return null;
+        String select = "select * from product;";
+        List<Product> products = new ArrayList<>();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(select)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id_product");
+                String name = resultSet.getString("name");
+                double price = resultSet.getDouble("price");
+                double sale_price = resultSet.getDouble("sale_price");
+                int quantity = resultSet.getInt("quantity");
+                String description = resultSet.getString("description");
+                String image = resultSet.getString("image");
+                int isActive = resultSet.getInt("isActive");
+                int id_category =resultSet.getInt("id_category");
+                int id_brand = resultSet.getInt("id_brand");
+                products.add(new Product(id, name, price, sale_price, quantity, description, image,isActive,id_category,id_brand));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return products;
     }
 
     @Override
--- src/main/java/com/example/case_study_md3_demo/controller/ProductServlet.java
+++ src/main/java/com/example/case_study_md3_demo/controller/ProductServlet.java
@@ -1,15 +1,27 @@
 package com.example.case_study_md3_demo.controller;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import com.example.case_study_md3_demo.DAO.iplm.ProductDAO;
+import com.example.case_study_md3_demo.model.Product;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "ProductServlet", value = "/products")
 public class ProductServlet extends HttpServlet {
+    ProductDAO productDAO = new ProductDAO();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        List<Product> products = productDAO.findAll();
+        request.setAttribute("products", products);
+        RequestDispatcher rq = request.getRequestDispatcher("display_product.jsp");
+        rq.forward(request, response);
     }
 
     @Override
